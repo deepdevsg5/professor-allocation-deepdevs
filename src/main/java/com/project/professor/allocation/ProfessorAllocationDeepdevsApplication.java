4936da53631616abc5ec3842b2f621f6c5f65e5e
deepdevsg5 <deepdevs.g5@gmail.com>
2023-07-06 21:00:10 -0300

    Packages adds

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationDeepdevsApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationDeepdevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationDeepdevsApplication.class, args);
+	}
+
+}
