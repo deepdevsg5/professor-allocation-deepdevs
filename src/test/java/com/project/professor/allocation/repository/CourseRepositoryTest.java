488e6030edde2aecd85a834fd0a1eeceb4b32fd0
Jacob Berenstein Neto <jbnlivriaria@gmail.com>
2023-07-14 18:17:55 -0300

    Final tests done

--- src/test/java/com/project/professor/allocation/repository/CourseRepositoryTest.java
+++ src/test/java/com/project/professor/allocation/repository/CourseRepositoryTest.java
@@ -1,4 +1,5 @@
 package com.project.professor.allocation.repository;
+
 import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.entity.Department;
 
@@ -18,21 +19,57 @@ import java.util.Optional;
 @Rollback(false)
 @TestPropertySource(locations = "classpath:application.properties")
 public class CourseRepositoryTest {
-	
-    @Autowired 
+
+	@Autowired
 	CourseRepository courseRepository;
 
-    
-    @Test
+	@Test
 	void create() {
-    	Course cur1 = new Course();
-    	cur1.setName("fisica");
-    	cur1.setId(null);
-    	
-    	Course cur2 = courseRepository.save(cur1);
-    	System.out.println(cur2);
-    	
-    	
-    }
-	
+		Course cur1 = new Course();
+		cur1.setName("Logica");
+		cur1.setId(null);
+
+		Course cur2 = courseRepository.save(cur1);
+		System.out.println(cur2);
+
+	}
+
+	@Test
+	void findAll() {
+
+		List<Course> courses = courseRepository.findAll();
+		System.out.println(courses);
+
+	}
+
+	@Test
+	void findByid() {
+		Long id = 2l;
+		Optional<Course> courses = courseRepository.findById(id);
+		Course cur = courses.orElse(null);
+		System.out.println(cur);
+	}
+
+	@Test
+	void update() {
+		Long id = 2l;
+		Course cur1 = new Course();
+		cur1.setName("Backend");
+		cur1.setId(id);
+
+		Course cur2 = courseRepository.save(cur1);
+		System.out.println(cur2);
+
+	}
+
+	@Test
+	void deleteById() {
+		long id = 4l;
+		courseRepository.deleteById(id);
+	}
+
+	@Test
+	void deleteAll() {
+		courseRepository.deleteAllInBatch();
+	}
 }
