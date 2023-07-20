61c213209c6d2f1884d289cf6ed141a65e1e27d5
Fabio Queiroz <antoniofabioqueiroz@gmail.com>
2023-07-19 21:39:08 -0300

    atualizando meu repo

---
+++ src/main/java/service/ProfessorService.java
@@ -0,0 +1,10 @@
+package service;
+
+public class ProfessorService {
+    
+    private ProfessorService repo;
+    private ProfessorService(ProfessorService repo){
+    this.repo = repo;        
+    }
+
+}
