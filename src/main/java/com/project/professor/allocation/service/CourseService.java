f0f0fd37e7f2d0676741ea5eacd0ef9f6447b422
Bruno D <qduttra@gmail.com>
2023-07-22 18:36:46 -0300

    CourseService "deleteAll" was missing
    parameter insert

--- src/main/java/com/project/professor/allocation/service/CourseService.java
+++ src/main/java/com/project/professor/allocation/service/CourseService.java
@@ -16,38 +16,48 @@ public class CourseService {
 
 		this.repo = repo;
 	}
-	public Course  findById(Long id) {
+
+	public Course findById(Long id) {
 		Optional<Course> findIdCourse = repo.findById(id);
 		Course course = findIdCourse.orElse(null);
 		return course;
 	}
-	public List<Course> findByName(String name) {
-	    return repo.findByNameContaining(name);
-	}
-	public List<Course> findAll() {
-	    List<Course> courses = repo.findAll();
-	    return courses;
-	}
-	public Course create(Course course) {
-	    course.setId(null);
-	    return saveInternal(course);
-	}  
-	public Course update(Course course) {
-	    Long courseId = course.getId();
 
-	    if (repo.existsById(courseId)) {
-	        return saveInternal(course);
-	    } else {
-	        return null;
-	    }
+	public List<Course> findByName(String name) {
+		return repo.findByNameContaining(name);
 	}
+
+	public List<Course> findAll() {
+		List<Course> courses = repo.findAll();
+		return courses;
+	}
+
+	public Course create(Course course) {
+		course.setId(null);
+		return saveInternal(course);
+	}
+
+	public Course update(Course course) {
+		Long courseId = course.getId();
+
+		if (repo.existsById(courseId)) {
+			return saveInternal(course);
+		} else {
+			return null;
+		}
+	}
+
 	public void deleteById(Long id) {
-	    repo.deleteById(id);
+		repo.deleteById(id);
 	}
+
+	public void deleteAll() {
+		repo.deleteAllInBatch();
+
+	}
+
 	private Course saveInternal(Course course) {
-	    Course savedCourse = repo.save(course);
-	    return savedCourse;
+		Course savedCourse = repo.save(course);
+		return savedCourse;
 	}
 }
-
-
