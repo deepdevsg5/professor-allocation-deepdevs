14db8a223924fee01cf55cb51e3aa3ad2906796d
Pjmaciel <pjmaciel_4@hotmail.com>
2023-07-26 19:16:01 -0300

    Add Anotation of Course

--- src/main/java/com/project/professor/allocation/controller/CourseController.java
+++ src/main/java/com/project/professor/allocation/controller/CourseController.java
@@ -25,6 +25,7 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    @Operation(summary = "Search Courses by Name")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Course>> findByName(@RequestParam(name = "name", required = false) String name) {
         List<Course> courses;
@@ -37,6 +38,7 @@ public class CourseController {
         
     }
 
+    @Operation(summary = "Search Course by ID")
     @GetMapping(path = "/{course_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Course> findById(@PathVariable(name = "course_id") Long id) {
         Course course = courseService.findById(id);
@@ -54,6 +56,7 @@ public class CourseController {
         return new ResponseEntity<>(createdCourse, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update Course")
     @PutMapping(path = "/{course_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Course> update(@PathVariable(name = "course_id") Long id,
                                          @RequestBody Course course) {
@@ -66,12 +69,19 @@ public class CourseController {
         }
     }
 
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "Course deleted successfully")
+    })
+    @Operation(summary = "Delete Course by ID")
     @DeleteMapping(path = "/{course_id}")
     public ResponseEntity<Void> deleteById(@PathVariable(name = "course_id") Long id) {
         courseService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "All Courses deleted successfully")
+    })
     @Operation(summary = "Delete All Courses")
     @DeleteMapping
     public ResponseEntity<Void> deleteAll() {
